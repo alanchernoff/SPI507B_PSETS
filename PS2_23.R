4154787ec6505cc6ecab21e426268edd9606d998
alanchernoff <90869944+alanchernoff@users.noreply.github.com>
2023-09-28 21:43:33 -0400

    Update PS2_23.R

--- PS2_23.R
+++ PS2_23.R
@@ -6,11 +6,14 @@ StateInc <- read.csv('StateIncome.csv')
 View(StateInc)
 
 # calculate cov between income in 2021 and 2022, fill in the xxxxx
-mean(StateInc$Income21,xxxxx)
+# xxxx = StateInc$Income22
+cov(StateInc$Income21,xxxxx)
 
-# calculate cov between income in 2021 and 2022, fill in the xxxxx
-median(StateInc$Income21,xxxxx)
+# calculate cor between income in 2021 and 2022, fill in the xxxxx
+# xxxx = StateInc$Income22
+cor(StateInc$Income21,xxxxx)
 
 # calculate regression using income in 2022 as the dependent variable, fill in the xxxxx
+# xxxx = StateInc$Income22
 OLS_1 <- lm(xxxxx ~ Income21, data = StateInc)
 summary(OLS_1)
